0090dd7ae8a6104a320b17c08496858c83603ea5
Jacek Łakomy <jac.lakomy@gmail.com>
2023-01-26 19:06:19 +0100

    zadanie nr5

--- challengeapp1/challengeapp1/Program.cs
+++ challengeapp1/challengeapp1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int myNumber = 159512132;
+string myNumberAsString = myNumber.ToString();
+char[] digits = myNumberAsString.ToCharArray();
+
+List<char> numbers = new List<char>();
+
+numbers.Add('0');
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+
+Console.WriteLine("Wyniki dla liczby 159512132");
+
+foreach (char number in numbers)
+{
+    int counter = 0;
+    foreach (char i in digits)
+    {
+        if (i == number) counter++;
+    }
+    Console.WriteLine(number + " => " + counter);
+}
+
+
+
